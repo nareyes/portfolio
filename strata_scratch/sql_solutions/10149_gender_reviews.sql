6c15cad6079c47ff60bff9fdb43dee18c2717804
nareyes <nick.reyes@sebrands.com>
2022-09-01 12:52:31 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/10149_gender_reviews.sql
@@ -0,0 +1,16 @@
+/*
+Write a query to find which gender gives a higher average review score when writing reviews as guests. Use the from_type column to identify guest reviews. Output the gender and their average review score.
+
+https://platform.stratascratch.com/coding/10149-gender-with-generous-reviews?code_type=1
+*/
+
+select
+    g.gender,
+    avg (review_score) as avg_score
+from airbnb_guests as g
+    inner join airbnb_reviews as r
+    on g.guest_id = r.from_user
+where r.from_type = 'guest'
+group by g.gender
+order by avg_score desc
+limit 1;
