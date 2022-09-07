3d207c8411c7ab36820bc6e01f06bb40df41cfc8
Nick Reyes <78861580+nareyes@users.noreply.github.com>
2022-09-07 09:00:03 -0500

    Update 9703_common_grade.sql

--- strata_scratch/sql_solutions/9703_common_grade.sql
+++ strata_scratch/sql_solutions/9703_common_grade.sql
@@ -6,21 +6,18 @@ https://platform.stratascratch.com/coding/9703-find-the-most-common-grade-earned
 
 
 with
-    grade_freq
-    as
-    (
-        select
-            grade,
-            count(*) as freq
-        from los_angeles_restaurant_health_inspections
-        where facility_name
-     ilike '%bakery%'
-        group by grade
+grade_freq as (
+    select
+        grade,
+        count(*) as freq
+    from los_angeles_restaurant_health_inspections
+    where facility_name ilike '%bakery%'
+    group by grade
 )
 
 select grade
 from grade_freq
 where freq = (
     select max(freq)
-from grade_freq
+    from grade_freq
     );
