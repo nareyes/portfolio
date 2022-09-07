c1454bab977dc20d9668e6ef992a0c93b325e360
nareyes <nick.reyes@sebrands.com>
2022-09-07 08:42:19 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/9703_common_grade.sql
@@ -0,0 +1,26 @@
+/*
+Find the most common grade earned by bakeries.
+
+https://platform.stratascratch.com/coding/9703-find-the-most-common-grade-earned-by-bakeries?code_type=1
+*/
+
+
+with
+    grade_freq
+    as
+    (
+        select
+            grade,
+            count(*) as freq
+        from los_angeles_restaurant_health_inspections
+        where facility_name
+     ilike '%bakery%'
+        group by grade
+)
+
+select grade
+from grade_freq
+where freq = (
+    select max(freq)
+from grade_freq
+    );
