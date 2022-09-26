03099cf92e07ed0a3afff93cf0100277f63b0ba9
nareyes <nick.reyes@sebrands.com>
2022-09-26 10:20:42 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/easy/10164_adwords_earnings.sql
@@ -0,0 +1,12 @@
+/*
+Find the total AdWords earnings for each business type. 
+Output the business types along with the total earnings.
+
+https://platform.stratascratch.com/coding/10164-total-adwords-earnings?code_type=1
+*/
+
+select
+    business_type,
+    sum (adwords_earnings) as earnings
+from google_adwords_earnings
+group by business_type;
