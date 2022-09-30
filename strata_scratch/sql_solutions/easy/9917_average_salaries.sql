5cfdc694f9f12712fd69f118012d46cd48f47e1d
nareyes <nick.reyes@sebrands.com>
2022-09-30 11:46:06 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/easy/9917_average_salaries.sql
@@ -0,0 +1,13 @@
+/*
+Compare each employee's salary with the average salary of the corresponding department.
+Output the department, first name, and salary of employees along with the average salary of that department.
+
+https://platform.stratascratch.com/coding/9917-average-salaries?code_type=1
+*/
+
+select
+    department,
+    first_name,
+    salary,
+    avg (salary) over (partition by department) as avg
+from employee;
