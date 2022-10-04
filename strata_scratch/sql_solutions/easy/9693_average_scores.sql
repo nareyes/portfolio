43584b96c14a1e5d4caed7fc9166e4fa5e013ecd
nareyes <nick.reyes@sebrands.com>
2022-10-04 09:49:59 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/easy/9693_average_scores.sql
@@ -0,0 +1,13 @@
+/*
+Find the average score for grades A, B, and C.
+Output the results along with the corresponding grade (ex: 'A', avg(score)).
+
+https://platform.stratascratch.com/coding/9693-find-the-average-score-for-grades-a-b-and-c?code_type=1
+*/
+
+select
+    grade,
+    avg (score) as avg
+from los_angeles_restaurant_health_inspections
+group by grade
+order by grade asc;
