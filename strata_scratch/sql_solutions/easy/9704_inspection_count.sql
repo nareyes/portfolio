849884946e66dcd4cc60ad2200d3d427389e198d
nareyes <nick.reyes@sebrands.com>
2022-10-05 12:30:13 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/easy/9704_inspection_count.sql
@@ -0,0 +1,14 @@
+/*
+Find the number of inspections per day.
+Output the result along with the date of the activity.
+Order results based on the activity date in the ascending order.
+
+https://platform.stratascratch.com/coding/9704-find-the-number-of-inspections-per-day?code_type=1
+*/
+
+select
+    activity_date,
+    count (distinct record_id)
+from los_angeles_restaurant_health_inspections
+group by activity_date
+order by activity_date asc;
