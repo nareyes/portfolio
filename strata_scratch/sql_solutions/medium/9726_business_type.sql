578624e994591f8adfb0c7fe252a1d9f85202730
nareyes <nick.reyes@sebrands.com>
2022-10-21 11:40:53 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/medium/9726_business_type.sql
@@ -0,0 +1,20 @@
+/*
+Classify each business as either a restaurant, cafe, school, or other. 
+- A restaurant should have the word 'restaurant' in the business name. 
+- For cafes, either 'cafe', 'café', or 'coffee' can be in the business name. 
+- 'School' should be in the business name for schools. 
+- All other businesses should be classified as 'other'. 
+Output the business name and the calculated classification.
+
+https://platform.stratascratch.com/coding/9726-classify-business-type?code_type=1
+*/
+
+select
+    distinct business_name,
+    case
+        when business_name ilike '%restaurant%' then 'restaurant'
+        when business_name ilike '%school%' then 'school'
+        when business_name ilike any(array['%cafe%', '%café%', '%coffee%']) then 'cafe'
+        else 'other'
+    end as business_type
+from sf_restaurant_health_violations;
