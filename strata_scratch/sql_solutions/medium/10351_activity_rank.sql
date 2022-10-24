2be4bdeb547e942a4a0ac12e7eb8c99012d01bbe
nareyes <nick.reyes@sebrands.com>
2022-10-24 13:20:08 -0500

    Add File via Git

---
+++ strata_scratch/sql_solutions/medium/10351_activity_rank.sql
@@ -0,0 +1,17 @@
+/*
+Find the email activity rank for each user. Email activity rank is defined by the total number of emails sent. 
+The user with the highest number of emails sent will have a rank of 1, and so on. 
+Output the user, total emails, and their activity rank. Order records by the total emails in descending order. 
+Sort users with the same number of emails in alphabetical order.
+In your rankings, return a unique value (i.e., a unique rank) even if multiple users have the same number of emails.
+
+https://platform.stratascratch.com/coding/10351-activity-rank?code_type=1
+*/
+
+select
+    from_user,
+    count (*) as total_emails,
+    row_number() over (order by count (*) desc, from_user asc) as row_number
+from google_gmail_emails
+group by from_user
+order by total_emails desc, from_user asc;
