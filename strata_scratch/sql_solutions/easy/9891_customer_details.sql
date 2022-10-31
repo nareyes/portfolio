6979f63e6c79180c9d8880ab91b9e061f7edbb4e
nareyes <nick.reyes@sebrands.com>
2022-10-31 12:47:46 -0500

    Add File via Git

--- strata_scratch/sql_solutions/easy/9891_customer_details.sql
+++ strata_scratch/sql_solutions/easy/9891_customer_details.sql
@@ -1,7 +1,6 @@
 /*
 Find the details of each customer regardless of whether the customer made an order.
 Output the customer's first name, last name, and the city along with the order details.
-
 You may have duplicate rows in your results due to a customer ordering several of the same items.
 Sort records based on the customer's first name and the order details in ascending order.
 
@@ -9,13 +8,30 @@ https://platform.stratascratch.com/coding/9891-customer-details?code_type=1
 */
 
 select
-    first_name,
-    last_name,
-    city,
-    order_details
+    c.first_name,
+    c.last_name,
+    c.city,
+    o.order_details
 from customers as c
     left join orders as o
         on c.id = o.cust_id
-order by
-    first_name asc,
-    order_details asc;
+order by 
+    c.first_name asc, 
+    o.order_details asc;
+
+
+-- alternate solution (control return for customers without an exisiting order)
+select
+    c.first_name,
+    c.last_name,
+    c.city,
+    case
+        when o.order_details is null then 'no order'
+        else o.order_details
+    end as order_details
+from customers as c
+    left join orders as o
+        on c.id = o.cust_id
+order by 
+    c.first_name asc, 
+    o.order_details asc;
