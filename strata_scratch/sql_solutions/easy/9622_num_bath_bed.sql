dc0cee9dbd2cfe1b81c838579df1ef35c434d943
nareyes <nick.reyes@sebrands.com>
2022-11-04 07:56:11 -0500

    Add File via Git

--- strata_scratch/sql_solutions/easy/9622_num_bath_bed.sql
+++ strata_scratch/sql_solutions/easy/9622_num_bath_bed.sql
@@ -8,8 +8,8 @@ https://platform.stratascratch.com/coding/9622-number-of-bathrooms-and-bedrooms?
 select
     city,
     property_type,
-    avg (bathrooms) as n_bathrooms_avg,
-    avg (bedrooms) as n_bedrooms_avg
+    avg (cast (bathrooms as float)) as avg_bathrooms,
+    avg (cast (bedrooms as float)) as avg_bedrooms
 from airbnb_search_details
 group by city, property_type
 order by city, property_type;
