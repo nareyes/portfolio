b6b6fefb619e4ff79df0acc070895c609e29d846
nareyes <nick.reyes@sebrands.com>
2022-11-15 08:06:44 -0600

    Add File via Git

---
+++ strata_scratch/python_solutions/easy/9625_cheapest_property.py
@@ -0,0 +1,15 @@
+'''
+Find the price of the cheapest property for every city.
+
+https://platform.stratascratch.com/coding/9625-cheapest-properties?code_type=2
+'''
+
+# Import your libraries
+import pandas as pd
+import numpy as np
+
+# Start writing code
+cheapest_property = airbnb_search_details.groupby(['city'])['price'].min().reset_index()[['city', 'price']]
+result = cheapest_property.rename(columns = {'price': 'min_price'})
+
+result
--- strata_scratch/sql_solutions/easy/9913_order_details.sql
+++ strata_scratch/sql_solutions/easy/9913_order_details.sql
@@ -10,10 +10,10 @@ https://platform.stratascratch.com/coding/9913-order-details?code_type=1
 select
     c.first_name,
     o.order_date,
-    o.order_details, o.total_order_cost
+    o.order_details,
+    o.total_order_cost
 from customers as c
-    -- left join preserves customer side in the case of missing orders (for reproducibility)
     left join orders as o
         on c.id = o.cust_id
-where first_name in ('Jill', 'Eva')
-order by o.cust_id asc;
+where first_name in ('Eva', 'Jill')
+order by c.id asc;
